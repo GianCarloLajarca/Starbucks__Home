3245af62aa4bd110405501b16b1214fad16ff3de
GianCarloLajarca <158068391+GianCarloLajarca@users.noreply.github.com>
2024-03-18 21:52:15 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        dark:"#2b2b2b",
+        light:"#ffffff",
+        berde:"#006341",
+        grayishblue:"#d5d0ea",
+        cream:"#f2f0eb",
+        lightberde: "#cee8c9",
+        lightpink: "#F5CAC4",
+        orangestarbs: "#e99264",
+        accent: "#1e3932",
+      }
+    },
+  },
+  plugins: [],
+}
